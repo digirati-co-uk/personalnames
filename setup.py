4ce6f2a25cb172c935fff136f814bdee2319470e
Matt McGrattan <matt.mcgrattan@digirati.com>
2018-08-17 13:50:11 +0100

    Minor version increment.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="personalnames",
-    version="0.0.1",
+    version="0.0.2",
     author="Matt McGrattan",
     author_email="matt.mcgrattan@digirati.com",
     description="Generate variant forms for personal names.",
